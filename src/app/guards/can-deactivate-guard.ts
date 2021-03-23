b1ac3b37799fefd4259229e205f26709e62b1669
Filippo <filippo.comastri2@studio.unibo.it>
2021-03-23 15:11:15 +0100

    prova-com

--- src/app/guards/can-deactivate-guard.ts
+++ src/app/guards/can-deactivate-guard.ts
@@ -15,7 +15,7 @@ export class CanDeactivateGuard implements CanDeactivate<MainPageComponent> {
       return this.dialog
         .open(ConfirmDialogComponent, {
           data: {
-            message: 'Do you have unsaved changes? If you leave, without saving, your changes will be lost',
+            message: 'Do you have unsaved changes ? If you leave, without saving, your changes will be lost',
             ok: 'Leave',
             ko: 'Cancel'
           }
