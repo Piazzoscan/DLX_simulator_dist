df9fd78a507e990472999fa4d772ffdbb0bc1f2f
ubolakes <umberto.laghi@studio.unibo.it>
2022-05-16 23:18:57 +0200

    gestione bottoni pause/resume

--- src/app/services/diagram.service.ts
+++ src/app/services/diagram.service.ts
@@ -7,35 +7,58 @@ import { DLXDiagrams } from '../diagram/dlx.diagrams';
 export class DiagramService {
 
   dlxDiagrams: DLXDiagrams;
-  injector: Injector; //serve davvero?
+  injector: Injector;
+  pauseEnabled: boolean; //mi dice se il pulsante di pause è cliccabile
+  stopEnabled: boolean; //mi dice se il pulsante di stop è cliccabile
 
   constructor(injector: Injector, appRef: ApplicationRef) {
     this.injector = injector;
     this.dlxDiagrams = new DLXDiagrams(appRef);
+    this.stopEnabled = false;
+    this.pauseEnabled = false;
   }
 
   public resume(){
     this.dlxDiagrams.resume();
+    this.pauseEnabled = true;
   }
 
   public stop(){
     this.dlxDiagrams.stop();
+    this.stopEnabled = false;
+    this.pauseEnabled = false;
   }
 
   public pause(){
     this.dlxDiagrams.pause();
+    this.pauseEnabled = false;
+    this.stopEnabled = true;
   }
 
   public load(){
     this.dlxDiagrams.load();
+    this.pauseEnabled = true;
+    this.stopEnabled = true;
   }
 
   public store(){
     this.dlxDiagrams.store();
+    this.pauseEnabled = true;
+    this.stopEnabled = true;
   }
 
   public idle(){
     this.dlxDiagrams.idle();
+    this.pauseEnabled = true;
+    this.stopEnabled = true;
+  }
+
+  public isPauseEnabled(){
+    return this.pauseEnabled;
+  }
+
+  public isStopEnabled(){
+    return this.stopEnabled;
   }
 
 }
